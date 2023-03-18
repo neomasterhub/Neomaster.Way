fa25fe35579852f16ef209ba3c3f23b9a03bcbce
Neomaster <the@neomaster.net>
2023-03-18 22:52:34 +0300

    [Extensions.Exception] ExceptionExtensions

---
+++ .Net/NuGet/Neomaster.Extensions.Exception/ExceptionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using E = System.Exception;
+
+namespace Neomaster.Extensions.Exception
+{
+    public static class ExceptionExtensions
+    {
+        public static List<E> GetAllInnerExceptions(this E e, bool addAggregate = false, bool aggregateToSingle = false)
+        {
+            List<E> list = new List<E>();
+
+            if (e == null)
+            {
+                return list;
+            }
+
+            if (e is AggregateException ae)
+            {
+                if (addAggregate)
+                {
+                    list.Add(aggregateToSingle ? new E(ae.Message) : ae);
+                }
+
+                list.AddRange(ae.InnerExceptions.SelectMany(ie => ie.GetAllInnerExceptions(addAggregate, aggregateToSingle)));
+            }
+            else
+            {
+                list.Add(e);
+                list.AddRange(e.InnerException.GetAllInnerExceptions(addAggregate, aggregateToSingle));
+            }
+
+            return list;
+        }
+    }
+}
