9d167a73c150a17dd2e092ddd1d9c2fc3be41f6b
Neomaster <the@neomaster.net>
2023-03-18 23:01:13 +0300

    [Extensions.Exception] ExceptionExtensions  description

--- .Net/NuGet/Neomaster.Extensions.Exception/ExceptionExtensions.cs
+++ .Net/NuGet/Neomaster.Extensions.Exception/ExceptionExtensions.cs
@@ -7,6 +7,17 @@ namespace Neomaster.Extensions.Exception
 {
     public static class ExceptionExtensions
     {
+        /// <summary>
+        /// Retrieves all inner exceptions of the <paramref name="e"/> tree, including <see cref="AggregateException">aggregate exceptions</see>.
+        /// <code>e1[e2[e3,e4]] -> [e1,e2,e3,e4]</code>
+        /// </summary>
+        /// <param name="e">The root of the exception tree, which are returned as a list.</param>
+        /// <param name="addAggregate">If <c>true</c>, <see cref="AggregateException"/> nodes will be included, default is <c>false</c>.</param>
+        /// <param name="aggregateToSingle">If <c>true</c>, <see cref="E"/> exception will be included with <see cref="AggregateException.Message"/> instead of <see cref="AggregateException"/> node.</param>
+        /// <returns>
+        /// List of all the inner exceptions of the <paramref name="e"/> tree.
+        /// If <paramref name="e"/> is <c>null</c>, an empty list will be returned.
+        /// </returns>
         public static List<E> GetAllInnerExceptions(this E e, bool addAggregate = false, bool aggregateToSingle = false)
         {
             List<E> list = new List<E>();
