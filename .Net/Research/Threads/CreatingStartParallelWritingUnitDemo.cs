e563acbb6a43c455c6af07fb36f50284816a9f46
Neomaster <the@neomaster.net>
2023-03-19 16:42:13 +0300

    [Threads] CreatingStartParallelWritingUnitDemo

---
+++ .Net/Research/Threads/CreatingStartParallelWritingUnitDemo.cs
@@ -0,0 +1,52 @@
+﻿using TestEngine6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Threads;
+
+public class CreatingStartParallelWritingUnitDemo : UnitDemoBase
+{
+    private static string _threadsOutput = string.Empty;
+
+    public CreatingStartParallelWritingUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    private void WriteChar(object value)
+    {
+        while (true)
+        {
+            _threadsOutput += value.ToString();
+            Thread.Sleep(500);
+        }
+    }
+
+    private void WriteStar()
+    {
+        while (true)
+        {
+            _threadsOutput += "*";
+            Thread.Sleep(100);
+        }
+    }
+
+    [Fact(DisplayName = "Creating, start, parallel writing")]
+    public void Demo()
+    {
+        var th1 = new Thread(WriteStar);
+        var th2 = new Thread(new ParameterizedThreadStart(WriteChar));
+
+        th1.Start();
+        th2.Start("@");
+
+        int i = 50;
+        while (i-- > 0)
+        {
+            _threadsOutput += ".";
+            Thread.Sleep(20);
+        }
+
+        Output.WriteLine(_threadsOutput); // *.@...*...*....*...*..@.*...*...*...*...*.@..*....*...*...*...@*
+    }
+}
