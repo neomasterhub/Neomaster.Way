2709047795c09f74d062758a1f4196dfd6d1986e
Neomaster <the@neomaster.net>
2023-03-19 13:43:20 +0300

    [StandardsTests6] GetAllInnerExceptionsUnitTests: "Should return single" region

--- .Net/Tests/StandardsTests6/Extensions/ExceptionExtensions/GetAllInnerExceptionsUnitTests.cs
+++ .Net/Tests/StandardsTests6/Extensions/ExceptionExtensions/GetAllInnerExceptionsUnitTests.cs
@@ -1,4 +1,4 @@
-#pragma warning disable SA1124
+#pragma warning disable SA1124, SA1116
 using Neomaster.Extensions.Exception;
 
 namespace StandardsTests6.Extensions.ExceptionExtensions;
@@ -52,6 +52,108 @@ public class GetAllInnerExceptionsUnitTests
         var actual = tree.GetAllInnerExceptions();
 
         Assert.Empty(actual);
+    }
+    #endregion
+
+    #region Should return single
+    [Fact(DisplayName = "e -> [e]")]
+    public void ShouldReturnSingle_1()
+    {
+        var expectedSingle = new Exception();
+
+        var actual = expectedSingle.GetAllInnerExceptions();
+
+        Assert.Single(actual);
+        Assert.Equal(expectedSingle, actual.Single());
+    }
+
+    [Fact(DisplayName = "ae -> [ae]")]
+    public void ShouldReturnSingle_2()
+    {
+        var expectedSingle = new AggregateException();
+
+        var actual = expectedSingle.GetAllInnerExceptions(addAggregate: true);
+        var actualSingle = actual.Single();
+
+        Assert.Single(actual);
+        Assert.IsType<AggregateException>(actualSingle);
+        Assert.Equal(expectedSingle, actualSingle);
+    }
+
+    [Fact(DisplayName = "ae -> [e]")]
+    public void ShouldReturnSingle_3()
+    {
+        var expectedSingleMessage = "error";
+
+        var actual = new AggregateException(expectedSingleMessage).GetAllInnerExceptions(
+            addAggregate: true,
+            aggregateToSingle: true);
+        var actualSingle = actual.Single();
+
+        Assert.Single(actual);
+        Assert.IsNotType<AggregateException>(actualSingle);
+        Assert.Equal(nameof(Exception), actualSingle.GetType().Name);
+        Assert.Equal(expectedSingleMessage, actualSingle.Message);
+    }
+
+    [Fact(DisplayName = "e[ae] -> [e]")]
+    public void ShouldReturnSingle_4()
+    {
+        var actual = new Exception(null, new AggregateException()).GetAllInnerExceptions();
+
+        Assert.Single(actual);
+        Assert.Equal(nameof(Exception), actual.Single().GetType().Name);
+    }
+
+    [Fact(DisplayName = "ae[e] -> [e]")]
+    public void ShouldReturnSingle_5()
+    {
+        var actual = new AggregateException(new Exception()).GetAllInnerExceptions();
+
+        Assert.Single(actual);
+        Assert.Equal(nameof(Exception), actual.Single().GetType().Name);
+    }
+
+    [Fact(DisplayName = "e[ae1[ae2]] -> [e]")]
+    public void ShouldReturnSingle_6()
+    {
+        var tree =
+            new Exception(null,
+                new AggregateException(
+                    new AggregateException()));
+
+        var actual = tree.GetAllInnerExceptions();
+
+        Assert.Single(actual);
+        Assert.Equal(nameof(Exception), actual.Single().GetType().Name);
+    }
+
+    [Fact(DisplayName = "ae1[ae2[e]] -> [e]")]
+    public void ShouldReturnSingle_7()
+    {
+        var tree =
+            new AggregateException(
+                new AggregateException(
+                    new Exception()));
+
+        var actual = tree.GetAllInnerExceptions();
+
+        Assert.Single(actual);
+        Assert.Equal(nameof(Exception), actual.Single().GetType().Name);
+    }
+
+    [Fact(DisplayName = "ae1[e[ae2]] -> [e]")]
+    public void ShouldReturnSingle_8()
+    {
+        var tree =
+            new AggregateException(
+                new Exception(null,
+                    new AggregateException()));
+
+        var actual = tree.GetAllInnerExceptions();
+
+        Assert.Single(actual);
+        Assert.Equal(nameof(Exception), actual.Single().GetType().Name);
     }
     #endregion
 }
