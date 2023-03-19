774b429b877c8bb1373772dc6939a3128289cf8b
Neomaster <the@neomaster.net>
2023-03-19 20:24:45 +0300

    [Thread] IsAliveUnitDemo

---
+++ .Net/Research/Threads/IsAliveUnitDemo.cs
@@ -0,0 +1,30 @@
+﻿using TestEngine6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Threads;
+
+public class IsAliveUnitDemo : UnitDemoBase
+{
+    public IsAliveUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    [Fact(DisplayName = "IsAlive in different thread life states")]
+    public void Demo()
+    {
+        var th = new Thread(() => Thread.Sleep(200));
+
+        Output.WriteLine(th.IsAlive.ToString()); // False
+
+        th.Start();
+        Thread.Sleep(100);
+
+        Output.WriteLine(th.IsAlive.ToString()); // True
+
+        Thread.Sleep(300);
+
+        Output.WriteLine(th.IsAlive.ToString()); // False
+    }
+}
