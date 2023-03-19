3729403fadaa2786b624d9c8890ff5a3018abc62
Neomaster <the@neomaster.net>
2023-03-19 13:32:37 +0300

    [StandardsTests6] GetAllInnerExceptionsUnitTests summary

--- .Net/Tests/StandardsTests6/Extensions/ExceptionExtensions/GetAllInnerExceptionsUnitTests.cs
+++ .Net/Tests/StandardsTests6/Extensions/ExceptionExtensions/GetAllInnerExceptionsUnitTests.cs
@@ -3,6 +3,12 @@ using Neomaster.Extensions.Exception;
 
 namespace StandardsTests6.Extensions.ExceptionExtensions;
 
+/// <summary>
+/// <c>e</c> - <see cref="Exception"/> instance,<br/>
+/// <c>ae</c> - <see cref="AggregateException"/> instance,<br/>
+/// <c>[e1,e2,e3]</c> - list,<br/>
+/// <c>e1[e2,e3]</c> - root with children.
+/// </summary>
 public class GetAllInnerExceptionsUnitTests
 {
     #region Should return empty
