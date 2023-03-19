02100c16fbd41db6cf17d448f338b66e37840d3c
Neomaster <the@neomaster.net>
2023-03-19 20:49:11 +0300

    [Threads] JoinUnitDemo

---
+++ .Net/Research/Threads/JoinUnitDemo.cs
@@ -0,0 +1,38 @@
+﻿using TestEngine6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Threads;
+
+public class JoinUnitDemo : UnitDemoBase
+{
+    public JoinUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    [Fact(DisplayName = "Sequential writing by joined threads")]
+    public void Demo()
+    {
+        var output = string.Empty;
+        var action = (char arg) =>
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                output += arg;
+            }
+        };
+        var th1 = new Thread(() => action.Invoke('*'));
+        var th2 = new Thread(() =>
+        {
+            th1.Join();
+            action.Invoke('.');
+        });
+
+        th1.Start();
+        th2.Start();
+        th2.Join();
+
+        Output.WriteLine(output); // *****.....
+    }
+}
--- README.md
+++ README.md
@@ -7,9 +7,11 @@
 3. [Foreground][threads-3]
 4. [Thread info: id, name, culture, is background][threads-4]
 5. [`IsAlive` in different thread life states][threads-5]
+6. [`Join()`, Sequential writing by joined threads][threads-6]
 
 [threads-1]:.Net/Research/Threads/CreationStartParallelWritingUnitDemo.cs
 [threads-2]:.Net/Research/Threads.Background/Program.cs
 [threads-3]:.Net/Research/Threads.Foreground/Program.cs
 [threads-4]:.Net/Research/Threads/InfoUnitDemo.cs
 [threads-5]:.Net/Research/Threads/IsAliveUnitDemo.cs
+[threads-6]:.Net/Research/Threads/JoinUnitDemo.cs
