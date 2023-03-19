5467fca704161c084d030ba34d6eae1d50c86690
Neomaster <the@neomaster.net>
2023-03-19 18:56:31 +0300

    [Research] Threads.Foreground

---
+++ .Net/Research/Threads.Foreground/Program.cs
@@ -0,0 +1,17 @@
+﻿var foreground = new Thread(() =>
+{
+    var i = 5;
+    while (i-- > 0)
+    {
+        Console.Write('F');
+        Thread.Sleep(1000);
+    }
+});
+
+Console.WriteLine($"is background: {foreground.IsBackground}"); // is background: False
+
+foreground.Start();
+
+Console.Write('M');
+
+// Output: MFFFFF
