5c5e62d054719a5c3becb28f94340e1ecc9bc3bb
Neomaster <the@neomaster.net>
2023-03-19 18:45:35 +0300

    [Research] Threads.Background

---
+++ .Net/Research/Threads.Background/Program.cs
@@ -0,0 +1,18 @@
+﻿var background = new Thread(() =>
+{
+    var i = 5;
+    while (i-- > 0)
+    {
+        Console.Write('B');
+        Thread.Sleep(1000);
+    }
+})
+{
+    IsBackground = true, // !
+};
+
+background.Start();
+
+Console.Write('M');
+
+// Output: MB
