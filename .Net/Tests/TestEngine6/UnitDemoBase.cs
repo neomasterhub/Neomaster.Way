a6a2a6db04af9640b74a38d1a5b1cab0d2a2dd9b
Neomaster <the@neomaster.net>
2023-03-19 16:30:51 +0300

    [TestEngine6] UnitDemoBase

---
+++ .Net/Tests/TestEngine6/UnitDemoBase.cs
@@ -0,0 +1,13 @@
+﻿using Xunit.Abstractions;
+
+namespace TestEngine6;
+
+public abstract class UnitDemoBase
+{
+    protected readonly ITestOutputHelper Output;
+
+    public UnitDemoBase(ITestOutputHelper output)
+    {
+        Output = output;
+    }
+}
