ae229f411c800138e1b2d567a241f05d036ee0ac
Neomaster <the@neomaster.net>
2023-03-29 13:33:32 +0300

    [Threads.AffinityParametrized] Counter

---
+++ .Net/Research/Threads.AffinityParametrized/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Threads.AffinityParametrized
+{
+    internal class Counter
+    {
+        private static bool _stop;
+        private readonly string _name;
+
+        public Counter(string name)
+        {
+            _name = name;
+        }
+
+        public void Run()
+        {
+            long i;
+
+            for (i = 0; !_stop && i < 1e9; i++) ;
+
+            _stop = true;
+
+            Console.WriteLine("Thread {0,12}, i={1}", _name, i);
+        }
+    }
+}
