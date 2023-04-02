2fb3095a4621f2db37f36ddb634a63b98a3cfe83
Neomaster <the@neomaster.net>
2023-04-02 16:22:38 +0300

    [Threads.Pool] Counter

---
+++ .Net/Research/Threads.Pool/ActivityTypes/Counter.cs
@@ -0,0 +1,31 @@
+﻿namespace Threads.Pool.ActivityTypes;
+
+public class Counter
+{
+    private int _count = 0;
+    public readonly int _to;
+
+    public Counter(CancellationToken cancellationToken, int to = 100)
+    {
+        CancellationToken = cancellationToken;
+        _to = to;
+    }
+
+    public int Count => _count;
+    public CancellationToken CancellationToken { get; set; }
+    public void CountAsync()
+    {
+        _count++;
+
+        for (int i = 0; i < _to; i++)
+        {
+            if (CancellationToken.IsCancellationRequested)
+            {
+                break;
+            }
+
+            _count++;
+            Thread.Sleep(10);
+        }
+    }
+}
