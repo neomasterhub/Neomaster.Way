f5c698b59ecc324599475fd8d63efc90041259ba
Neomaster <the@neomaster.net>
2023-04-20 01:30:49 +0300

    [Tasks] TaskUnitDemo

---
+++ .Net/Research/Tasks/TaskUnitDemo.cs
@@ -0,0 +1,41 @@
+﻿using TestEngine6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tasks;
+
+public class TaskUnitDemo : UnitDemoBase
+{
+    private static string _resource = string.Empty;
+
+    public TaskUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    private static void CallbackCore(object state)
+    {
+        while (true)
+        {
+            _resource += state;
+            Thread.Sleep(10);
+        }
+    }
+
+    [Fact]
+    public void Demo()
+    {
+        WaitCallback callback1 = state => CallbackCore(state);
+        Action<object> callback2 = state => CallbackCore(state);
+
+        Task.Run(() => callback1('-'));
+        new Task(callback2, '|').Start();
+
+        Thread.Sleep(100);
+
+        Output.WriteLine(_resource);
+
+        // Output:
+        // |--||--||--
+    }
+}
