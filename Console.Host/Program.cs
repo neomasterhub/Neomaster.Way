ad4bbfff9791146bae3b19a0d5063eed42166140
Neomaster <the@neomaster.net>
2023-04-20 20:18:45 +0300

    [Console.Host] Program

---
+++ Console.Host/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices((hostContext, services) =>
+    {
+        // ... hostContext.Configuration ...
+        services.AddScoped<IFoo, Foo>();
+    })
+    .Build();
+
+var foo = builder.Services.GetRequiredService<IFoo>();
+
+Console.WriteLine(foo.Get()); // Bar
+
+builder.Run();
