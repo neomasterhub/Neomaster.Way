c21267eef7f91423449f31e5af4152d611bce624
Neomaster <the@neomaster.net>
2023-04-20 20:02:56 +0300

    [Console.Host] Service

---
+++ Console.Host/Service.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Console.Host;
+
+public interface IFoo
+{
+    string Get();
+}
+
+internal class Foo : IFoo
+{
+    private readonly IConfiguration _configuration;
+
+    public Foo(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string Get() => _configuration["Foo"];
+}
