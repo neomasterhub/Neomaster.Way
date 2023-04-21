f3475114284141023987662839390c2ecaf9fb23
Neomaster <the@neomaster.net>
2023-04-21 17:26:17 +0300

    [XUnitTools] GetCurrentDayService

---
+++ .Net/Research/XUnitTools/ActivityTypes/GetCurrentDayService.cs
@@ -0,0 +1,13 @@
+﻿namespace XUnitTools.ActivityTypes;
+
+internal class GetCurrentDayService : IGetCurrentDayService
+{
+    private readonly IGetCurrentDatetimeService _getCurrentDatetimeService;
+
+    public GetCurrentDayService(IGetCurrentDatetimeService getCurrentDatetimeService)
+    {
+        _getCurrentDatetimeService = getCurrentDatetimeService;
+    }
+
+    public int Get() => _getCurrentDatetimeService.Get().Day;
+}
