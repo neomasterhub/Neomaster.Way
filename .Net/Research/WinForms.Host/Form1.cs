2af932ab51b033107968adb1289cb99f573041dd
Neomaster <the@neomaster.net>
2023-04-21 12:51:17 +0300

    WinForms.Host project

---
+++ .Net/Research/WinForms.Host/Form1.cs
@@ -0,0 +1,9 @@
+namespace WinForms.Host;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ .Net/Research/WinForms.Host/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WinForms.Host;
+
+namespace DataManager;
+
+internal static class Program
+{
+    public static IConfiguration Configuration;
+
+    [STAThread]
+    private static void Main()
+    {
+        Configuration = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+        var services = new ServiceCollection();
+
+        services
+            .AddScoped<Form1>();
+
+        using ServiceProvider serviceProvider = services.BuildServiceProvider();
+        var form1 = serviceProvider.GetRequiredService<Form1>();
+        Application.Run(form1);
+    }
+}
