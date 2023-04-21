eb687c835306945a1f4cf9823dff69250b0b2505
Neomaster <the@neomaster.net>
2023-04-21 16:04:57 +0300

    [XUnitTools] BoxComparer

---
+++ .Net/Research/XUnitTools/ActivityTypes/BoxComparer.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace XUnitTools.ActivityTypes;
+
+internal class BoxComparer : IEqualityComparer<Box>
+{
+    public bool Equals(Box x, Box y)
+    {
+        return x.Color == y.Color && x.Size == y.Size;
+    }
+
+    public int GetHashCode([DisallowNull] Box obj) => obj.GetHashCode();
+}
