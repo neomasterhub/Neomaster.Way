100fbbfff018fda09e2e3926cd0a712dc9275d4b
Neomaster <the@neomaster.net>
2023-04-21 16:45:50 +0300

    [XUnitTools] SameUnitTests.Objects()

---
+++ .Net/Research/XUnitTools/SameUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using XUnitTools.ActivityTypes;
+
+namespace XUnitTools;
+
+public class SameUnitTests
+{
+    [Fact]
+    public void Objects()
+    {
+        var o1 = new Box("red", 1);
+        var o2 = new Box("red", 1);
+
+        Assert.NotSame(o1, o2);
+    }
+}
