93587001b1baddc107139a05a521d3767a162db7
Neomaster <the@neomaster.net>
2023-04-21 16:06:39 +0300

    [XUnitTools] EqualUnitTests

---
+++ .Net/Research/XUnitTools/EqualUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using XUnitTools.ActivityTypes;
+
+namespace XUnitTools;
+
+public class EqualUnitTests
+{
+    [Fact]
+    public void Objects()
+    {
+        var obj1 = new Box("red", 1);
+        var obj2 = new Box("red", 1);
+
+        Assert.Equal(obj1, obj2, new BoxComparer());
+        Assert.NotEqual(obj1, obj2);
+    }
+}
