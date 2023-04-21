df4670567c7caa9f93b70f5212416bcd39dfe231
Neomaster <the@neomaster.net>
2023-04-21 20:41:30 +0300

    [XUnitTools] AutoMockUnitTests fix

--- .Net/Research/XUnitTools/AutoMockUnitTests.cs
+++ .Net/Research/XUnitTools/AutoMockUnitTests.cs
@@ -7,39 +7,26 @@ namespace XUnitTools;
 
 public class AutoMockUnitTests
 {
-    private static readonly int[] _expected = { 10, 20 };
-    private static int _expectedIndex;
     private readonly AutoMock _autoMock;
 
     public AutoMockUnitTests()
     {
-        var sutMock = new Mock<IGetCurrentDayService>();
-        var expected = _expected[_expectedIndex];
+        var sutMock = new Mock<IGetCurrentDayService>(MockBehavior.Strict);
         sutMock
             .Setup(m => m.Get())
-            .Returns(expected);
+            .Returns(1);
 
         _autoMock = AutoMock.GetLoose(builder =>
         {
             builder.RegisterMock(sutMock);
         });
-
-        _expectedIndex++;
     }
 
     [Fact]
-    public void Test1()
+    public void Test()
     {
         var actual = _autoMock.Mock<IGetCurrentDayService>().Object.Get();
 
-        Assert.Equal(10, actual);
-    }
-
-    [Fact]
-    public void Test2()
-    {
-        var actual = _autoMock.Mock<IGetCurrentDayService>().Object.Get();
-
-        Assert.Equal(20, actual);
+        Assert.Equal(1, actual);
     }
 }
