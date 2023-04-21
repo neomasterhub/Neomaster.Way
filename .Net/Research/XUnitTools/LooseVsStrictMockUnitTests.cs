b757e74c2771a5c52e9e9fce935fd04081850af9
Neomaster <the@neomaster.net>
2023-04-21 17:41:18 +0300

    [XUnitTools] LooseVsStrictMockUnitTests

---
+++ .Net/Research/XUnitTools/LooseVsStrictMockUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using Xunit;
+using XUnitTools.ActivityTypes;
+
+namespace XUnitTools;
+
+public class LooseVsStrictMockUnitTests
+{
+    [Fact]
+    public void Loose()
+    {
+        var sutMock = new Mock<IGetCurrentDayService>(MockBehavior.Loose);
+        var sut = sutMock.Object;
+
+        var actual = sut.Get();
+
+        Assert.Equal(default(int), actual);
+    }
+
+    [Fact]
+    public void Strict()
+    {
+        var sutMock = new Mock<IGetCurrentDayService>(MockBehavior.Strict);
+        var sut = sutMock.Object;
+
+        var actual = Record.Exception(() => sut.Get());
+
+        Assert.IsType<MockException>(actual);
+
+        // 'IGetCurrentDayService.Get() invocation failed with mock behavior Strict.
+        // All invocations on the mock must have a corresponding setup.'
+    }
+}
