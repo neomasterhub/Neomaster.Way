dcce66df9172cc7f324376a431c066c713a90406
Neomaster <the@neomaster.net>
2023-04-23 09:45:56 +0300

    [DomainEventsArch] DEA.L3.Infrastructure.EmailProvider

---
+++ .Net/Research/DomainEventsArch/DEA.L3.Infrastructure.EmailProvider/EmailProvider.cs
@@ -0,0 +1,25 @@
+﻿using DEA.L1.Domain.Model.Entities;
+using DEA.L1.Domain.Model.Providers;
+using MediatR;
+
+namespace DEA.L3.Infrastructure.EmailProvider;
+
+internal class EmailProvider : IEmailProvider
+{
+    private readonly IMediator _mediator;
+
+    public EmailProvider(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public void Send(Email email)
+    {
+        foreach (var e in email.ReadEvents())
+        {
+            _mediator.Publish(e);
+        }
+
+        // external.Sent(email);
+    }
+}
---
+++ .Net/Research/DomainEventsArch/DEA.L3.Infrastructure.EmailProvider/Registrar.cs
@@ -0,0 +1,12 @@
+﻿using DEA.L1.Domain.Model.Providers;
+using DEA.L2.ApplicationServices.EmailManager;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DEA.L3.Infrastructure.EmailProvider;
+
+public static class Registrar
+{
+    public static IServiceCollection AddEmailProvider(this IServiceCollection services) => services
+        .AddEmailManager()
+        .AddScoped<IEmailProvider, EmailProvider>();
+}
