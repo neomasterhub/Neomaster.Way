abb714a4be45bc20ee7ae0ec2c7996afe878a062
Neomaster <the@neomaster.net>
2023-04-23 09:25:01 +0300

    [DomainEventsArch] DEA.L0.PlatformExtension

---
+++ .Net/Research/DomainEventsArch/DEA.L0.PlatformExtension/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace DEA.L0.PlatformExtension;
+
+public static class Logger
+{
+    public static void WriteLineObjectDescription(string description, object obj)
+    {
+        var objTypeName = obj.GetType().Name;
+
+        Console.WriteLine(description);
+        Console.Write($"<{objTypeName}>");
+        Console.WriteLine(JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true }));
+        Console.WriteLine();
+    }
+}
