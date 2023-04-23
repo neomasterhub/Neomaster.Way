32d0e82536134144114d8c780e77c7d292b09dec
Neomaster <the@neomaster.net>
2023-04-23 09:33:01 +0300

    [DomainEventsArch] DEA.L2.ApplicationServices.Audit

---
+++ .Net/Research/DomainEventsArch/DEA.L2.ApplicationServices.Audit/AnyEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using DEA.L0.PlatformExtension;
+using DEA.L1.Domain.Model.Events;
+using MediatR;
+
+namespace DEA.L2.ApplicationServices.Audit;
+
+public class AnyEventHandler<T> : INotificationHandler<T>
+    where T : Event
+{
+    public Task Handle(T e, CancellationToken cancellationToken)
+    {
+        Logger.WriteLineObjectDescription("Audit event:", e);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ .Net/Research/DomainEventsArch/DEA.L2.ApplicationServices.Audit/Regisatrar.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DEA.L2.ApplicationServices.Audit;
+
+public static class Regisatrar
+{
+    public static IServiceCollection AddAudit(this IServiceCollection services) => services
+        .AddMediatR(conf => conf.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+}
