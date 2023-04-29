9c2e71b05192f09caed4d708e04ff3c6fb17a496
Neomaster <the@neomaster.net>
2023-04-29 11:23:27 +0300

    [Tasks] AsyncParallelTasksDemo

---
+++ .Net/Research/Tasks/AsyncParallelTasksUnitDemo.cs
@@ -0,0 +1,47 @@
+﻿using TestEngine6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tasks;
+
+public class AsyncParallelTasksUnitDemo : UnitDemoBase
+{
+    private static readonly Action<char> _endlessWriter = c =>
+    {
+        while (true)
+        {
+            Thread.Sleep(10);
+            _resource += c;
+        }
+    };
+    private static string _resource = string.Empty;
+
+    public AsyncParallelTasksUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    private static async Task Async1()
+    {
+        await Task.Run(() => _endlessWriter('.'));
+    }
+
+    private static async Task Async2()
+    {
+        await Task.Run(() => _endlessWriter('*'));
+    }
+
+    [Fact]
+    public void Demo()
+    {
+        _ = Async1();
+        _ = Async2();
+
+        Thread.Sleep(200);
+
+        Output.WriteLine(_resource);
+
+        // Output:
+        // **.*..***...*.*.**.
+    }
+}
