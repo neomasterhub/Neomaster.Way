0d8d1224ce0cefa90fa196e4309c2fe78a7a5b67
Neomaster <the@neomaster.net>
2023-04-29 12:27:36 +0300

    [Tasks] AsyncTaskVsVoidExceptionUnitDemo

---
+++ .Net/Research/Tasks/AsyncTaskVsVoidExceptionUnitDemo.cs
@@ -0,0 +1,49 @@
+﻿using TestEngine6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tasks;
+
+public class AsyncTaskVsVoidExceptionUnitDemo : UnitDemoBase
+{
+    public AsyncTaskVsVoidExceptionUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    private static async void AsyncVoid()
+    {
+        await Task.Run(() => throw new ArithmeticException());
+    }
+
+    private static async Task AsyncTask()
+    {
+        await Task.Run(() => throw new ArithmeticException());
+    }
+
+    [Fact(DisplayName = "Should be failed")]
+    public void Demo()
+    {
+        try
+        {
+            _ = AsyncTask();
+        }
+        catch (ArithmeticException)
+        {
+            Output.WriteLine("task ex");
+        }
+
+        try
+        {
+            AsyncVoid();
+        }
+        catch (ArithmeticException)
+        {
+            Output.WriteLine("void ex");
+        }
+
+        // Output:
+        // System.ArithmeticException : Overflow or underflow in the arithmetic operation.
+        // ... line 16
+    }
+}
