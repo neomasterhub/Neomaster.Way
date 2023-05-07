7e4a5baf580105e6001bdc2e9b973a5234c0752f
Neomaster <the@neomaster.net>
2023-05-07 12:19:53 +0300

    [WinForms.ScopedVsTransient] Types

---
+++ .Net/Research/WinForms.ScopedVsTransient/Types.cs
@@ -0,0 +1,48 @@
+﻿namespace WinForms.ScopedVsTransient;
+
+internal interface IService
+{
+    string GetInfo();
+}
+
+internal interface IScopedService : IService
+{
+}
+
+internal interface ITransientService : IService
+{
+}
+
+internal class Service : IScopedService, ITransientService
+{
+    private readonly Guid _guid;
+
+    public Service()
+    {
+        _guid = Guid.NewGuid();
+    }
+
+    public string GetInfo()
+    {
+        return _guid.ToString();
+    }
+}
+
+internal class Client
+{
+    private readonly IScopedService _scoped;
+    private readonly ITransientService _transient;
+
+    public Client(IScopedService scoped, ITransientService transient)
+    {
+        _scoped = scoped;
+        _transient = transient;
+    }
+
+    public void PrintServicesInfo()
+    {
+        Console.WriteLine($"Scoped    : {_scoped.GetInfo()}");
+        Console.WriteLine($"Transient : {_transient.GetInfo()}");
+        Console.WriteLine();
+    }
+}
