3f27248a13d01ae61bae6485316d8d32645a89af
Neomaster <the@neomaster.net>
2023-05-07 11:37:09 +0300

    Console.ScopedVsTransient project

---
+++ .Net/Research/Console.ScopedVsTransient/Types.cs
@@ -0,0 +1,29 @@
+﻿namespace Console.ScopedVsTransient;
+
+internal interface IService
+{
+    string GetInfo();
+}
+
+internal interface IScopedService : IService
+{
+}
+
+internal interface ITransientService : IService
+{
+}
+
+internal class Service : IScopedService, ITransientService
+{
+    private readonly Guid _guid;
+
+    public Service()
+    {
+        _guid = Guid.NewGuid();
+    }
+
+    public string GetInfo()
+    {
+        return _guid.ToString();
+    }
+}
