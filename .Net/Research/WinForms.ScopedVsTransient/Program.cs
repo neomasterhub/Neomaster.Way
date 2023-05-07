8ec886f533ede73060728d76675749978257e68a
Neomaster <the@neomaster.net>
2023-05-07 12:23:01 +0300

    [WinForms.ScopedVsTransient] Services have been registered.

--- .Net/Research/WinForms.ScopedVsTransient/Program.cs
+++ .Net/Research/WinForms.ScopedVsTransient/Program.cs
@@ -1,16 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace WinForms.ScopedVsTransient;
 
 internal static class Program
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
     [STAThread]
-    static void Main()
+    private static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) => services
+                .AddScoped<Form1>()
+                .AddScoped<IScopedService, Service>()
+                .AddTransient<ITransientService, Service>()
+                .AddTransient<Client>())
+            .Build();
+
+        Application.Run(host.Services.GetRequiredService<Form1>());
     }
 }
