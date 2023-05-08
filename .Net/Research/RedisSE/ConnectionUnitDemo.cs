abbba8898f8cb4ca49a8aac9ddc23b05834730c6
Neomaster <the@neomaster.net>
2023-05-08 16:14:52 +0300

    [RedisSE] ConnectionUnitDemo

--- .Net/Research/RedisSE/ConnectionUnitDemo.cs
+++ .Net/Research/RedisSE/ConnectionUnitDemo.cs
@@ -1,4 +1,7 @@
-﻿using TestEngine6;
+﻿using System.Text.Json;
+using StackExchange.Redis;
+using TestEngine6;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace RedisSE;
@@ -8,5 +11,27 @@ public class ConnectionUnitDemo : UnitDemoBase
     public ConnectionUnitDemo(ITestOutputHelper output)
         : base(output)
     {
+    }
+
+    [Fact]
+    public void Demo()
+    {
+        var redis = ConnectionMultiplexer.Connect("localhost");
+
+        Output.WriteLine(JsonSerializer.Serialize(redis, new JsonSerializerOptions { WriteIndented = true }));
+
+        // Output:
+        // {
+        //   "PreserveAsyncOrder": false,
+        //   "IncludeDetailInExceptions": true,
+        //   "IncludePerformanceCountersInExceptions": false,
+        //   "TimeoutMilliseconds": 5000,
+        //   "ClientName": "PC-WHITE(SE.Redis-v2.6.111.64013)",
+        //   "Configuration": "localhost",
+        //   "IsConnected": true,
+        //   "IsConnecting": false,
+        //   "OperationCount": 12,
+        //   "StormLogThreshold": 15
+        // }
     }
 }
