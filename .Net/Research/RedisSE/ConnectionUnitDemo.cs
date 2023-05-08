3973ce14e96cbe04631daddb848450256c458944
Neomaster <the@neomaster.net>
2023-05-08 16:08:10 +0300

    [RedisSE] ConnectionUnitDemo billet

---
+++ .Net/Research/RedisSE/ConnectionUnitDemo.cs
@@ -0,0 +1,12 @@
+﻿using TestEngine6;
+using Xunit.Abstractions;
+
+namespace RedisSE;
+
+public class ConnectionUnitDemo : UnitDemoBase
+{
+    public ConnectionUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+}
