acb21058abb11730ba4161c9571999fb399f606b
Neomaster <the@neomaster.net>
2023-05-09 22:23:42 +0300

    [XUnitTools] AsyncLifetimeFixture constructor has been added.

--- .Net/Research/XUnitTools/ActivityTypes/AsyncLifetimeFixture.cs
+++ .Net/Research/XUnitTools/ActivityTypes/AsyncLifetimeFixture.cs
@@ -4,17 +4,22 @@ namespace XUnitTools.ActivityTypes;
 
 public class AsyncLifetimeFixture : IAsyncLifetime
 {
-    private static readonly List<int> _arr = new();
+    private static List<int> _arr;
+
+    public AsyncLifetimeFixture()
+    {
+        _arr = new List<int>();
+    }
 
     public IReadOnlyCollection<int> Arr => _arr;
-
-    public async Task DisposeAsync()
-    {
-        await Task.Run(() => _arr.Clear());
-    }
 
     public async Task InitializeAsync()
     {
         await Task.Run(() => _arr.Add(1));
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Task.Run(() => _arr.Clear());
     }
 }
