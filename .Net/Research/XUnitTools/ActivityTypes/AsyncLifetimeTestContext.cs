eafbceb2fd0311d7c5e3f6d8d34011dab3ab63f3
Neomaster <the@neomaster.net>
2023-05-09 21:35:07 +0300

    [XUnitTools] AsyncLifetimeTestContext

---
+++ .Net/Research/XUnitTools/ActivityTypes/AsyncLifetimeTestContext.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace XUnitTools.ActivityTypes;
+
+internal class AsyncLifetimeTestContext : IAsyncLifetime
+{
+    private readonly List<int> _arr = new();
+
+    public async Task DisposeAsync()
+    {
+        await Task.Run(() => _arr.Clear());
+    }
+
+    public async Task InitializeAsync()
+    {
+        await Task.Run(() => _arr.Add(1));
+    }
+}
