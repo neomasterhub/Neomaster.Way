999ae544989cd60c801093b9a63ab65cd10e00eb
Neomaster <the@neomaster.net>
2023-05-09 22:05:20 +0300

    [XUnitTools] FixtureUnitTests

---
+++ .Net/Research/XUnitTools/FixtureUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using XUnitTools.ActivityTypes;
+
+namespace XUnitTools;
+
+public class FixtureUnitTests : IClassFixture<AsyncLifetimeFixture>
+{
+    private readonly AsyncLifetimeFixture _fixture;
+
+    public FixtureUnitTests(AsyncLifetimeFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void Test1() => Assert.Equal(1, _fixture.Count);
+
+    [Fact]
+    public void Test2() => Assert.Equal(1, _fixture.Count);
+
+    [Fact]
+    public void Test3() => Assert.Equal(1, _fixture.Count);
+}
