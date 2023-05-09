acf9e0b41da260a637d73b88201de98d72ecfe55
Neomaster <the@neomaster.net>
2023-05-09 23:12:57 +0300

    [XUnitTools] Stacks

---
+++ .Net/Research/XUnitTools/ActivityTypes/Stacks.cs
@@ -0,0 +1,11 @@
+﻿namespace XUnitTools.ActivityTypes;
+
+internal class Stacks : IDisposable
+{
+    public static List<int> Ints = new();
+
+    public void Dispose()
+    {
+        Ints.Clear();
+    }
+}
