ba854498ae9a07fc483e4563870ee6087286a0c4
Neomaster <the@neomaster.net>
2023-05-09 23:15:55 +0300

    [XUnitTools] DisposableTestingUnitTests billet

---
+++ .Net/Research/XUnitTools/DisposableTestingUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using XUnitTools.ActivityTypes;
+
+namespace XUnitTools;
+
+public class DisposableTestingUnitTests : IDisposable
+{
+    private readonly Stacks _stacks;
+
+    public DisposableTestingUnitTests()
+    {
+        _stacks = new Stacks();
+    }
+
+    public void Dispose()
+    {
+        _stacks.Dispose();
+    }
+}
