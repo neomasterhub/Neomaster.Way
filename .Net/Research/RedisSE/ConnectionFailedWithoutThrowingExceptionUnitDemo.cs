efe2e2937a2d336475a33b00538549c8664201aa
Neomaster <the@neomaster.net>
2023-06-04 19:49:43 +0300

    [RedisSE] Connection failed without throwing exception

---
+++ .Net/Research/RedisSE/ConnectionFailedWithoutThrowingExceptionUnitDemo.cs
@@ -0,0 +1,38 @@
+﻿using StackExchange.Redis;
+using TestEngine6;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RedisSE;
+
+public class ConnectionFailedWithoutThrowingExceptionUnitDemo : UnitDemoBase
+{
+    public ConnectionFailedWithoutThrowingExceptionUnitDemo(ITestOutputHelper output)
+        : base(output)
+    {
+    }
+
+    [Fact]
+    public void Demo()
+    {
+        var getConnection = () => ConnectionMultiplexer.Connect("host", options =>
+        {
+            options.ConnectTimeout = 0;
+            options.AbortOnConnectFail = false;
+        });
+
+        try
+        {
+            getConnection.Invoke();
+
+            Output.WriteLine("no exception");
+        }
+        catch
+        {
+            Output.WriteLine("exception");
+        }
+
+        // Output:
+        // no exception
+    }
+}
