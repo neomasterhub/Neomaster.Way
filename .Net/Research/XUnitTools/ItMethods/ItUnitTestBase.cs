be62bb8f7c25c9864702938d55ed82a2f5ed14e4
Neomaster <the@neomaster.net>
2024-03-04 00:42:41 +0300

    [ It ] ItUnitTestBase

---
+++ .Net/Research/XUnitTools/ItMethods/ItUnitTestBase.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Extras.Moq;
+using Moq;
+using XUnitTools.ItMethods.ActivityTypes;
+
+namespace XUnitTools.ItMethods;
+
+public class ItUnitTestBase
+{
+    protected readonly IService2 Sut;
+    protected readonly Mock<IService1> Mock;
+
+    public ItUnitTestBase()
+    {
+        Mock = new Mock<IService1>();
+
+        var autoMock = AutoMock.GetLoose(
+            builder =>
+            {
+                builder.RegisterMock(Mock);
+                builder.RegisterType<Service2>().As<IService2>();
+            });
+
+        Sut = autoMock.Create<IService2>();
+    }
+}
