58b96eff43f070f61cf7a4fa34cfc6c1cb4d8d37
Neomaster <the@neomaster.net>
2024-03-04 00:48:32 +0300

    [ It ] IsAnyUnitTests

---
+++ .Net/Research/XUnitTools/ItMethods/IsAnyUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using Xunit;
+
+namespace XUnitTools.ItMethods;
+
+public class IsAnyUnitTests : ItUnitTestBase
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void Test(int? arg)
+    {
+        var anyArgHasBeenPassedToService1 = false;
+        Mock
+            .Setup(m => m.Get1(It.IsAny<int?>()))
+            .Callback(() => anyArgHasBeenPassedToService1 = true);
+
+        Sut.Get2(arg);
+
+        // 2 checks that the callback has been called:
+        Assert.True(anyArgHasBeenPassedToService1);
+        Mock.Verify(m => m.Get1(It.IsAny<int?>()), Times.Once);
+    }
+}
