51aa86802f8080f13059d181b41175c5d8a6da47
Neomaster <the@neomaster.net>
2024-03-04 09:26:29 +0300

    [ It ] IsNotNullUnitTests

---
+++ .Net/Research/XUnitTools/ItMethods/IsNotNullUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using Xunit;
+
+namespace XUnitTools.ItMethods;
+
+public class IsNotNullUnitTests : ItUnitTestBase
+{
+    [Fact]
+    public void Test1()
+    {
+        var notNullHasBeenPassedToService1 = false;
+        Mock
+            .Setup(m => m.Get1(It.IsNotNull<int?>()))
+            .Callback(() => notNullHasBeenPassedToService1 = true);
+
+        Sut.Get2(1);
+
+        Assert.True(notNullHasBeenPassedToService1);
+        Mock.Verify(m => m.Get1(It.IsNotNull<int?>()), Times.Once);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var notNullHasBeenPassedToService1 = false;
+        Mock
+            .Setup(m => m.Get1(It.IsNotNull<int?>()))
+            .Callback(() => notNullHasBeenPassedToService1 = true);
+
+        Sut.Get2(null);
+
+        Assert.False(notNullHasBeenPassedToService1);
+        Mock.Verify(m => m.Get1(It.IsNotNull<int?>()), Times.Never);
+    }
+}
